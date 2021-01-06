691011bcadea16fc71211be2fae849418e21567b
emcruise <emcruise@protonmail.com>
2021-01-06 01:09:51 +0100

    Command Line args parsing handled in seperate file.

---
+++ listener/commandLineArgs.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+
+def parse():
+	parser = ArgumentParser(description='Python3 Tor Rootkit Listener')
+	parser.add_argument('lport', type=int, help='The port the hidden service should listen on.')
+	parser.add_argument('fport', type=int, help='The port the hidden service shoudl forward to.')
+	args = parser.parse_args()
+	return args.lport, args.fport
--- listener/listener.py
+++ listener/listener.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
 from network import ListenerSocket, TorHiddenService
 from shell import ListenerShell
 from style import Style
+import commandLineArgs
 import os
 import sys
 
@@ -13,24 +13,12 @@ It handles all other modules of the listener package.
 class Listener(Style):
     def __init__(self):
         self.checkRunningAsRoot()
-        lport, fport = Listener.getCommandLineArgs()
+        lport, fport = commandLineArgs.parse()
         torHS = TorHiddenService('listener', lport, fport)
         listenerSocket = ListenerSocket(fport)
         listenerSocket.start()
         shell = ListenerShell(listenerSocket)
         shell.run()
-
-    """
-    Returns the command line arguments of the programm call.
-    Example: ./listener.py 8080 8080
-    """
-    @staticmethod
-    def getCommandLineArgs() -> int:
-        parser = ArgumentParser(description='Python3 Tor Rootkit Listener')
-        parser.add_argument('lport', type=int, help='The port the hidden service should listen on.')
-        parser.add_argument('fport', type=int, help='The port the hidden service shoudl forward to.')
-        args = parser.parse_args()
-        return args.lport, args.fport
 
     """
     Check if the programm runs as root.
