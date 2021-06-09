c873d5b7c4e0f5dc558acc1fbd232b5380a0c5f4
emcruise <76820025+emcruise@users.noreply.github.com>
2021-06-09 19:26:34 +0200

    Update build.py
    Update

--- client/build.py
+++ client/build.py
@@ -12,7 +12,7 @@ def getTorExpertBundle():
 	os.chdir('torbundle')
 	
 	# download tor expert bundle
-	torURL = 'https://www.torproject.org/dist/torbrowser/10.0.15/tor-win32-0.4.5.7.zip'
+	torURL = 'https://www.torproject.org/dist/torbrowser/10.0.17/tor-win32-0.4.5.8.zip'
 	fileData = requests.get(torURL, allow_redirects=True)
 
 	# write downloaded tor expert bundle
