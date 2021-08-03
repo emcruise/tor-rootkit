e1f69b37ddfc39e6ac630dd7120081577ce9ea91
emcruise <e.e@e>
2021-08-03 19:47:26 +0200

    Testing outline

---
+++ tests/listener/socket_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class SocketTest(unittest.TestCase):
+    def __init__(self):
+        # Start Listener
+
+    def test_socket_closed_properly(self):
+        pass
+
+    def test_tor_process_closed_properly(self):
+        pass
+
+    def test_accept_connections(self):
+        pass
