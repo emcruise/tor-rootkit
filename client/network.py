a086f8d1c04655f1b8c504451b76d26966e15db9
emcruise <emcruise@mac>
2021-10-12 12:50:21 +0200

    Some debugging and regeneration of releases

--- client/network.py
+++ client/network.py
@@ -57,7 +57,8 @@ class ClientSocket:
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(self.remote_addr)
-        except socket.error:
+        except socket.error as err:
+            print(err)
             # randomize connection timeout to avoid network detection
             timeout = randint(self.CONNECTION_TIMEOUT - 10, self.CONNECTION_TIMEOUT + 10)
             sleep(timeout)
