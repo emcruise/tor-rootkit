d46dc06bd891092fdd8cb432f97da1b70b742d0e
emcruise <emcruise@mac>
2021-10-22 15:19:15 +0200

    Testing

---
+++ tests/unit_tests/listener/_test_client_binaries.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestNetworkConnectivity(unittest.TestCase):
+    def test_file_size(self):
+        pass
+
+    def test_meta_data(self):
+        pass
---
+++ tests/unit_tests/listener/_test_network.py
@@ -0,0 +1,11 @@
+import unittest
+import socket
+import requests
+
+
+class TestNetworkConnectivity(unittest.TestCase):
+    def test_loopback(self):
+        pass
+
+    def test_onion(self):
+        pass
